e691b1150993fba4a379bc6022538c2342ca1218
vbeersma <vbeersma@gmail.com>
2013-12-12 10:08:44 +0100

    setStatusForSBI kleiner gemaakt

--- scrumboardwidgethandler.cpp
+++ scrumboardwidgethandler.cpp
@@ -20,47 +20,37 @@ bool ScrumboardWidgetHandler::setStatusForSBI(ItemUI *item, LaneUI *lane){
     ScrumboardWidget *scrumboard = dynamic_cast<ScrumboardWidget *>(parent);
     QString laneName = scrumboard->compareLane(lane);
     if(laneName != QString("Undefined")){
-        SBIVisitor visitor;
-        for(int i = 0; i < tfs.getSelectedSprint()->getWorkItemArray().size(); i++){
-            WorkItem *workitem = tfs.getSelectedSprint()->getWorkItem(i);
-            if(workitem)
-                workitem->accept(visitor);
-        }
-        vector<SprintBacklogItem*> &SBIlist = visitor.getList();
-        for(vector<SprintBacklogItem*>::const_iterator it = SBIlist.begin(); it != SBIlist.end(); ++it){
-            SprintBacklogItem *SBIitem = *it;
-            QString itemID = item->getID();
-            itemID.remove(0,1);
-            if(SBIitem->getWorkItemNumber() == itemID.toInt()){
-                StatusType::ItemStorage::iterator iType;
-                for(iType = StatusType::getStorage().begin(); iType != StatusType::getStorage().end(); ++iType){
-                    pair<std::string, StatusType*> x = *iType;
-                    StatusType *st = x.second;
-                    if(st->getName() == laneName){
-                        if(acceptStatus(SBIitem->getStatus(SBIitem->sizeStatus() -1)->getStatusType()->getName(), laneName)){
-                            if(laneName == "Done"){
-                                RemainingWorkHistory *history = new RemainingWorkHistory();
-                                history->setDay(QDate::currentDate().day());
-                                history->setMonth(QDate::currentDate().month());
-                                history->setYear(QDate::currentDate().year());
-                                SBIitem->addRemainingWorkHistory(*history);
-                            }
-                            Status *status = new Status();
-                            status->setStatusType(*st);
-                            status->setDay(QDate::currentDate().day());
-                            status->setMonth(QDate::currentDate().month());
-                            status->setYear(QDate::currentDate().year());
-                            SBIitem->addStatus(*status);
-                            tfs.saveSelectedProject();
-                            return true;
-                        }else{
-                            return false;
+        QString itemID = item->getID().remove(0,1);
+
+        SprintBacklogItem* SBIitem = this->getItemForID(itemID.toInt());
+        if(SBIitem){
+            StatusType::ItemStorage::iterator iType;
+            for(iType = StatusType::getStorage().begin(); iType != StatusType::getStorage().end(); ++iType){
+                pair<std::string, StatusType*> x = *iType;
+                StatusType *st = x.second;
+                if(st->getName() == laneName){
+                    if(acceptStatus(SBIitem->getStatus(SBIitem->sizeStatus() -1)->getStatusType()->getName(), laneName)){
+                        if(laneName == "Done"){
+                            RemainingWorkHistory *history = new RemainingWorkHistory();
+                            history->setDay(QDate::currentDate().day());
+                            history->setMonth(QDate::currentDate().month());
+                            history->setYear(QDate::currentDate().year());
+                            SBIitem->addRemainingWorkHistory(*history);
                         }
+                        Status *status = new Status();
+                        status->setStatusType(*st);
+                        status->setDay(QDate::currentDate().day());
+                        status->setMonth(QDate::currentDate().month());
+                        status->setYear(QDate::currentDate().year());
+                        SBIitem->addStatus(*status);
+                        tfs.saveSelectedProject();
+                        return true;
+                    }else{
+                        return false;
                     }
                 }
             }
         }
-
     }else{
         //exception LaneUI name does not exist!
         return false;
